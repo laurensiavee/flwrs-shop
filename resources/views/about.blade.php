17177768e7dffc07c13068833398e9117d279407
laurensiavee <92752405+laurensiavee@users.noreply.github.com>
2023-06-19 13:34:27 +0700

    update name

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -6,8 +6,8 @@
     <div class="my-5 py-3">
         <div class="mh-center my-5 py-3">
             <div class="ctr-70 mh-center">
-                <h3 class="text-center fw-bold font-thicc"> "Toko Kembang grew from our love for flower and to spread the love in our community, we started off from street vendor near Rawa Belong street and now we have the most diverse collecton for plants and we provide professional nstallation and ongoing maintenance of your live plants."</h3>
-                <p class="text-center mt-4"><b>Nanda ria /</b> CEO. Tokem</p>
+                <h3 class="text-center fw-bold font-thicc"> "Flwrs. we choose flowers, for you."</h3>
+                <p class="text-center mt-4"><b>- /</b> CEO. Flwrs</p>
             </div>
         </div>
     </div>
@@ -20,19 +20,19 @@
             <div class="ctr-3">
                 <div class="mb-3">
                     <p class="mp-0"><b>Sales Inquiry</b></p>
-                    <p class="mp-0 text-secondary">sales@tokem.com</p>
+                    <p class="mp-0 text-secondary">sales@flwrs.com</p>
                     <p class="mp-0 text-secondary">+62 (021) 840-7279 ext 1</p>
                 </div>
                 <div class="mb-3">
                     <p class="mp-0"><b>B2B</b></p>
-                    <p class="mp-0 text-secondary">b2b@tokem.com</p>
+                    <p class="mp-0 text-secondary">b2b@flwrs.com</p>
                     <p class="mp-0 text-secondary">+62 (021) 840-7279 ext 6</p>
                 </div>
             </div>
             <div class="ctr-3">
                 <div class="mb-3">
                     <p class="mp-0"><b>Computer Service</b></p>
-                    <p class="mp-0 text-secondary">cs@tokem.com</p>
+                    <p class="mp-0 text-secondary">cs@flwrs.com</p>
                     <p class="mp-0 text-secondary">+62 (021) 840-7279 ext 9</p>
                 </div>
             </div>
@@ -47,25 +47,25 @@
             <div class="ctr-3">
                 <div class="mb-3">
                     <p class="mp-0"><b>Jakarta</b></p>
-                    <p class="mp-0 text-secondary">Jl. Rawa Belong No. 420</p>
-                    <p class="mp-0 text-secondary">11420</p>
+                    <p class="mp-0 text-secondary">Jl. lorem No. 0</p>
+                    <p class="mp-0 text-secondary">00000</p>
                 </div>
                 <div class="mb-3">
                     <p class="mp-0"><b>Karawang</b></p>
-                    <p class="mp-0 text-secondary">Jl. Belakang Pasar No. 14</p>
-                    <p class="mp-0 text-secondary">41311</p>
+                    <p class="mp-0 text-secondary">Jl. lorem No. 14</p>
+                    <p class="mp-0 text-secondary">00000</p>
                 </div>
             </div>
             <div class="ctr-3">
                 <div class="mb-3">
                     <p class="mp-0"><b>Bandung</b></p>
-                    <p class="mp-0 text-secondary">Jl. Dr. Setiabudi No. 269</p>
-                    <p class="mp-0 text-secondary">40154</p>
+                    <p class="mp-0 text-secondary">Jl. Dr. lorem No. 269</p>
+                    <p class="mp-0 text-secondary">00000</p>
                 </div>
                 <div class="mb-3">
                     <p class="mp-0"><b>Surabaya</b></p>
-                    <p class="mp-0 text-secondary">Jl. Ngaget No. 173</p>
-                    <p class="mp-0 text-secondary">60246</p>
+                    <p class="mp-0 text-secondary">Jl. lorem No. 173</p>
+                    <p class="mp-0 text-secondary">00000</p>
                 </div>
             </div>
         </div>
